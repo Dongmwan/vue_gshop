a17f73df7e14e8d0c7adf35d73966463c46d6303
DESKTOP-7H77TPO\Maura <824902361@qq.com>
2025-07-22 21:15:53 +0800

    10_尚硅谷_Vue项目_各导航路由组件(静态)

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="footer_guide border-1px">
-    <span href="javascript:;" class="guide_item" :class="{on:'/msite' === $route.path}" @click="goTo('/msite')">
+    <span class="guide_item" :class="{on:'/msite' === $route.path}" @click="goTo('/msite')">
       <span class="item_icon">
         <i class="iconfont icon-waimai"></i>
       </span>
       <span>外卖</span>
     </span>
-    <a href="javascript:;" class="guide_item" :class="{on:'/search' === $route.path}" @click="goTo('/search')">
+    <span class="guide_item" :class="{on:'/search' === $route.path}" @click="goTo('/search')">
       <span class="item_icon">
         <i class="iconfont icon-search"></i>
       </span>
       <span>搜索</span>
-    </a>
-    <a href="javascript:;" class="guide_item" :class="{on:'/order' === $route.path}" @click="goTo('/order')">
+    </span>
+    <span class="guide_item" :class="{on:'/order' === $route.path}" @click="goTo('/order')">
       <span class="item_icon">
         <i class="iconfont icon-dingdan"></i>
       </span>
       <span>订单</span>
-    </a>
-    <a href="javascript:;" class="guide_item" :class="{on:'/profile' === $route.path}" @click="goTo('/profile')">
+    </span>
+    <span class="guide_item" :class="{on:'/profile' === $route.path}" @click="goTo('/profile')">
       <span class="item_icon">
         <i class="iconfont icon-geren"></i>
       </span>
       <span>我的</span>
-    </a>
+    </span>
   </div>
 </template>
 
