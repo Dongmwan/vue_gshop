f54f73674278c91b7923270bd7ee0881b4e520bb
DESKTOP-7H77TPO\Maura <824902361@qq.com>
2025-07-23 14:41:15 +0800

    15_尚硅谷_Vue项目_启动后台应用并测试接口（无代码）16_尚硅谷_Vue项目_封装ajax请求函数

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "indent": ["error", tab]  // 改为 4 或 "tab" 如果你想用 4 空格
   }
 }
---
+++ src/api/ajsx.js
@@ -0,0 +1,47 @@
+/*
+ajax请求函数模块
+返回值：promise对象（异步返回的数据是：response.data） 
+*/
+import axios from 'axios'
+export default function ajax (url, data = {}, type = 'GET') {
+    return new Promise(function (resolve, reject) {
+/* 方法二：
+手动封装 Promise，可以自定义 resolve 和 reject 的行为。
+可以 对 axios 返回的数据进行额外处理（如只返回 response.data，而不是整个 response）。
+如果后续需要 增加额外逻辑（如缓存、日志等），可以在 new Promise 内部处理。
+ */
+        // 执行异步ajax请求
+        let promise
+        if (type === 'GET') {
+            // 准备url query参数数据
+            let dataStr = '' // 数据拼接字符串
+            Object.keys(data).forEach(key => {
+                dataStr += key + '=' + data[key] + '&'
+            })
+            if (dataStr !== '') {
+                dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+                url = url + '?' + dataStr
+            }
+            // 发送get请求
+            promise = axios.get(url) //返回的是response
+        } else {
+            // 发送post请求
+            promise = axios.post(url, data)
+        }
+        promise.then(function (response) {
+            // 成功了调用resolve()
+            resolve(response.data)
+        }).catch(function (error) {
+            // 失败了调用reject()
+            reject(error)
+        })
+    })
+}
+/*
+方法一：
+const response = await ajax()
+const result = response.data
+
+方法二：加return new Promise(function (resolve, reject) {}；resolve；reject
+const resule = await ajax()
+ */
