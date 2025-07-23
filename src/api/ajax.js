366a73f03143b05a9b94da4a69286b146c375975
DESKTOP-7H77TPO\Maura <824902361@qq.com>
2025-07-23 23:11:07 +0800

    20_尚硅谷_Vue项目_配置代理实现跨域ajax请求

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,15 +1,16 @@
 /*
 ajax请求函数模块
-返回值：promise对象（异步返回的数据是：response.data） 
+返回值：promise对象（异步返回的数据是：response.data）
 */
 import axios from 'axios'
 export default function ajax (url, data = {}, type = 'GET') {
     return new Promise(function (resolve, reject) {
-/* 方法二：
-手动封装 Promise，可以自定义 resolve 和 reject 的行为。
-可以 对 axios 返回的数据进行额外处理（如只返回 response.data，而不是整个 response）。
-如果后续需要 增加额外逻辑（如缓存、日志等），可以在 new Promise 内部处理。
- */
+        /*
+        方法二：
+        手动封装 Promise，可以自定义 resolve 和 reject 的行为。
+        可以 对 axios 返回的数据进行额外处理（如只返回 response.data，而不是整个 response）。
+        如果后续需要 增加额外逻辑（如缓存、日志等），可以在 new Promise 内部处理。
+         */
         // 执行异步ajax请求
         let promise
         if (type === 'GET') {
@@ -23,16 +24,16 @@ export default function ajax (url, data = {}, type = 'GET') {
                 url = url + '?' + dataStr
             }
             // 发送get请求
-            promise = axios.get(url) //返回的是response
+            promise = axios.get(url) // 返回的是response
         } else {
             // 发送post请求
             promise = axios.post(url, data)
         }
         promise.then(function (response) {
-            // 成功了调用resolve()
+            // 成功了调用 resolve()
             resolve(response.data)
         }).catch(function (error) {
-            // 失败了调用reject()
+            // 失败了调用 reject()
             reject(error)
         })
     })
